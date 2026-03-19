90047543a02765bd7815acaebe6e79ddba0b73cd
Kathryn Audrey <12405269+kaudreydev@users.noreply.github.com>
2026-03-19 14:15:21 -0400

    Implemented ShadCN, Tailwind, and Astro React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,21 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
+  image: {
+    domains: ["unpkg.com"],
+    remotePatterns: [{ protocol: "https" }],
+  },
+
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
       "Cross-Origin-Resource-Policy": "cross-origin",
       "Content-Security-Policy":
-        "default-src * https://astro-naut.statichost.eu/; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://astro-naut.statichost.eu/ data:",
+        "default-src * https://astro-naut.statichost.eu/; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://astro-naut.statichost.eu/ https://unpkg.com/ data:",
       "Permissions-Policy": "geolocation=(), camera=(), microphone=()",
       "Referrer-Policy": "strict-origin-when-cross-origin",
       "Upgrade-Insecure-Requests": "1",
@@ -23,4 +30,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
