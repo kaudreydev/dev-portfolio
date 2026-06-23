8be362819a23f775b336a1abe57a0793edac29e6
Kathryn Audrey <12405269+kaudreydev@users.noreply.github.com>
2026-06-23 12:19:34 -0400

    Added E2E test for dark mode switcher

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -1,7 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("meta is correct", async ({ page }) => {
-  await page.goto("http://localhost:4321/");
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
 
+test("meta is correct", async ({ page }) => {
   await expect(page).toHaveTitle("KAudreyDev");
+});
+
+test("switches to dark mode and back to light", async ({ page }) => {
+  await page.locator("[aria-label='dark mode']").click();
+
+  await expect(page.locator("html")).toHaveAttribute("class", "dark");
+
+  await page.locator("[aria-label='light mode']").click();
+
+  await expect(page.locator("html")).toHaveAttribute("class", "light");
 });
