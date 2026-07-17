a6edd4e1917a210331d3f59390b1a15e0ee70bc9
Kathryn Audrey <12405269+kaudreydev@users.noreply.github.com>
2026-07-17 15:58:28 -0400

    Testing workflow change

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm dev",
+    command: "pnpm dev --verbose",
     url: "http://localhost:4321",
     reuseExistingServer: !process.env.CI,
   },
